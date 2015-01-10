fbabd5a8ec7611bb12ec233f4b737a0d2d619322
Max Timje <ttiimmjjee@hotmail.com>
2015-01-10 08:53:14 +0100

    2015-01-10

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $app->get('/twig/{name}', function ($name) use ($app) {
         'name' => $name,
     ));
 });*/
+$urlPre ="http://devscout.mikaelsvensson.info:10081/api/v1/";
 ?>
 <!doctype html>
 <html>
@@ -16,14 +17,8 @@ $app->get('/twig/{name}', function ($name) use ($app) {
 <meta charset="utf-8">
 <title>Aktivitetsbank</title>
 <link rel="stylesheet" href="styles/style.css">
-<script type="text/javascript">
-    function updateTextInput1(val) {
-      document.getElementById('textInput1').value=val; 
-    }
-	function updateTextInput2(val) {
-      document.getElementById('textInput2').value=val; 
-    }
-  </script>
+<?php include 'javaScript/funktions.php'; ?>
+
 </head>
 
 <body>
@@ -38,31 +33,59 @@ $app->get('/twig/{name}', function ($name) use ($app) {
     <input type="text" id="textInput2" value="0">
 </form>
 <?php
-if (isset($_GET["sokfras"])){
+/* OM MAN GÖR EN NY SÖNKNIG START */
+if (isset($_GET["sokfras"]) or isset($_GET["age_1"]) or isset($_GET["age_2"]) ){
 	
 	$sokOrd =$_GET["sokfras"];
 	
 	if (isset($_GET["age_1"]) && $_GET["age_1"] != 0){
-		 $sokOrd=$sokOrd.$_GET["age_1"];
+		 $sokOrd=$sokOrd."&age_1=".$_GET["age_1"];
+		 if ($_GET["age_2"] == 0){
+			 $sokOrd=$sokOrd."&age_2=99";
+			 }
 	}
 	if (isset($_GET["age_2"]) && $_GET["age_2"] != 0){
-		 $sokOrd=$sokOrd.$_GET["age_2"];
+		 $sokOrd=$sokOrd."&age_2=".$_GET["age_2"];
 	}
 		$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-		$url="http://devscout.mikaelsvensson.info:10081/api/v1/activities?text=".$sokOrd;
+		$url=$urlPre."activities?text=".$sokOrd;
 		$contents = file_get_contents($url); 
 		$results = json_decode($contents); 
 		
+		$resultcount = count($results);
+		$sidcount = ($resultcount-1)/10;
+		$sidcount = ceil($sidcount);
+	for ($x = 1; $x <= $sidcount; $x++) {
+    	echo '<input type="button" id="button_show_more" value="'.$x.'" onclick="showResults('.$x.')">&nbsp;&nbsp;';
+	}
+		//echo $sokOrd;
+		$x=0;
 		foreach ($results as $post){
-			echo "<p>";
+			if ($x<=9){ $display = "style='display:block;'";}else { $display = "style='display:none;'";}
+			echo '<div id="'.$x.'"'.$display.' class="result" >';
 			echo "<h1> <a href='mer.php?id=".$post->id."'>".$post->name."</a></h1>";
 			echo " Från ".$post->age_min;
 			echo " till ".$post->age_max."år";
 			echo "<br/>";
-			echo $post->descr_introduction;
-			echo "</p>";
+			echo $post->descr_introduction; $x++;
+			echo "</div>";
+			
 	}
-	
+		$resultcount = count($results);
+	$sidcount = ($resultcount-1)/10;
+	$sidcount = ceil($sidcount);
+	?>
+    	<div>
+	<!--		<input type="button" id="button_show_past" value="Förra" onClick="showPast()">-->
+    <?php
+	for ($x = 1; $x <= $sidcount; $x++) {
+    	echo '<input type="button" id="button_show_more" value="'.$x.'" onclick="showResults('.$x.')">&nbsp;&nbsp;';
+	}
+	?>
+		<!--<input type="button" id="button_show_next" value="Nästa" onclick="showNext()> -->
+        </div>
+    <?php
+	echo "Antal sidor:".$sidcount;
 }else {
 	echo "skriv ett sök ord";
 }
